fc24815d2067f430f0000289dbe5daafef3ebbca
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2020-12-13 01:07:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,25 +28,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.array());
 app.use(express.static('public'));
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'uploads/');
-    },
 
-    // By default, multer removes file extensions so let's add them back
-    filename: function(req, file, cb) {
-        cb(null, __dirname + "/records/images/"+file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-});
 
-const imageFilter = function(req, file, cb) {
-    // Accept images only
-    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
-        req.fileValidationError = 'Only image files are allowed!';
-        return cb(new Error('Only image files are allowed!'), false);
-    }
-    cb(null, true);
-};
 
 app.get('/index', function (req, res) {
    console.log("Got a GET request for the homepage");
@@ -93,27 +76,8 @@ app.post('/donateus', function (req, res) {
     }
 });
 
-  let upload = multer({ storage: storage, fileFilter: imageFilter }).single('file');
-  upload(req, res, function(err) {
-        // req.file contains information of uploaded file
-        // req.body contains information of text fields, if there were any
 
-        if (req.fileValidationError) {
-            return res.send(req.fileValidationError);
-        }
-        else if (!req.file) {
-            return res.send('Please select an image to upload');
-        }
-        else if (err instanceof multer.MulterError) {
-            return res.send(err);
-        }
-        else if (err) {
-            return res.send(err);
-        }
 
-        // Display uploaded image for user validation
-        res.send(`You have uploaded this image: <hr/><img src="${req.file.path}" width="500"><hr /><a href="./">Upload another image</a>`);
-    });
 
     }
     else {
