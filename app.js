d9b4606cb783b75fb523d56a9ee151c93329c48a
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2020-12-12 23:29:36 +0530

    DonateS

--- app.js
+++ app.js
@@ -18,11 +18,13 @@ app.get('/donateus', function (req, res) {
    res.sendFile( __dirname + "/templates/" + "donateus.html" );
 })
 
-// This responds a POST request for the homepage
-app.post('/', function (req, res) {
-   console.log("Got a POST request for the homepage");
-   res.send('Hello POST');
+
+app.post('/donateus', function (req, res) {
+  const username = req.params.username
+  console.log("Got a POST request for the homepage"+username);
+  res.end()
 })
+
 
 
 var server = app.listen(3000, function () {
